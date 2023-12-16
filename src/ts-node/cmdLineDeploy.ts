8cebb958fceaff65cc56a1ebd9b04727a34679e9
Jeff Turner <1573697+jeffrey-l-turner@users.noreply.github.com>
2023-12-15 20:07:15 -0800

    try/catch not catching an await problem, promise not resolving

--- src/ts-node/cmdLineDeploy.ts
+++ src/ts-node/cmdLineDeploy.ts
@@ -1,28 +1,14 @@
 // @ts-check
 //  remember to run: `npm run rnode-generate`
-import * as R from 'ramda';
-import { ec } from 'elliptic';
-import * as protoLoader from '@grpc/proto-loader';
-import * as grpcLibrary from 'grpc';
-// import { addressCtrl } from '../web/controls/address-ctrl.js';
 import {
   rnodeDeploy, signDeploy, getAddrFromPrivateKey, // rnodePropose,  getAddrFromPublicKey, getAddrFromEth,
   verifyRevAddr, // rhoParToJson, verifyDeploy  newRevAddress, 
 } from '@tgrospic/rnode-grpc-js';
 
-//const { rnodeDeploy, rnodePropose, signDeploy, verifyDeploy, rhoParToJson } = require('@tgrospic/rnode-grpc-js')
-//import  { rnodeExample } from '../nodejs/client.js';
-
 import * as protoSchema from '../../rnode-grpc-gen/js/pbjs_generated.json';
-//import { encodeBase16, decodeBase16 } from '../lib.js';
-//import { verifyDeployEth, recoverPublicKeyEth } from '../eth/eth-sign.js';
-// import { ethDetected, ethereumAddress, ethereumSign, } from '../eth/eth-wrapper.js';
-// import { signDeploy, verifyDeploy, deployDataProtobufSerialize, } from '../rnode-sign'; */
 import * as grpc from '@grpc/grpc-js';
 
 const main = (args: string[]) => {
-  //console.log(args);
-  //console.log(args.length);
   const privKey = args.slice(2)[0];
   if (args.length < 3 || !privKey) {
     console.warn(`usage: ${args[0]}: must include private key`)
@@ -30,24 +16,15 @@ const main = (args: string[]) => {
     process.exit(1);
   }
 
-  //const packageDefinition = protoLoader.loadSync(
-    //{}
-  // );
-  //const packageObject = grpcLibrary.loadPackageDefinition(packageDefinition);
-  //console.warn('reading grpc Protobuf definitions...');
-  // console.dir(packageObject);
   const val = privKey.replace(/^0x/, '').trim()
   const keys = {
     privateKey: val,
     fromPriv_account: getAddrFromPrivateKey(val),
-    //fromPub:  getAddrFromPublicKey(val),
-    //fromEth:  getAddrFromEth(val),
     isRev: verifyRevAddr(val)
   };
   console.dir(keys);
-  // rnodeExample();
 
-  const rnodeInternalUrl = 'localhost:40402'
+  const rnodeInternalUrl = 'localhost:40402';
   const options = host => ({ grpcLib: grpc, host, protoSchema })
   const deployObj = rnodeDeploy(options(rnodeInternalUrl));
   const {
@@ -59,12 +36,20 @@ const main = (args: string[]) => {
     listenForDataAtName,
   } = deployObj;
 
-  console.dir(deployObj);
   grpcSignDeploy(keys, lastFinalizedBlock);
 };
 
 const grpcSignDeploy = async (keys, lastFinalizedBlock) => {
-  const lastBlockObj = await lastFinalizedBlock()
+  console.log('inside grpcSignDeploy...');
+  let lastBlockObj = {blockinfo: {blockinfo: {blocknumber: 0}}};
+  try { 
+    lastBlockObj = (lastBlockObj = await lastFinalizedBlock())
+  } catch(err) {
+    console.warn(`could not call lastFinalizedBlock(): ${err}`)
+  };
+  console.log('lastBlockObj:');
+  console.dir(lastBlockObj);
+
   const sampleRholangCode = `
     new return(\`rho:rchain:deployId\`), out(\`rho:io:stdout\`), x in {
     out!("Nodejs deploy test") |
@@ -82,7 +67,8 @@ const grpcSignDeploy = async (keys, lastFinalizedBlock) => {
     validafterblocknumber: lastBlockObj.blockinfo?.blockinfo.blocknumber || 0,
     shardid: 'root',
   }
-  console.dir(signDeploy(keys.privateKey, deployData));
+  const result = signDeploy(keys.privateKey, deployData);
+  console.log(result);
 }
 
 main(process.argv);
